a9c9954654ebb9f007ae37b74a84dfc114fc6516
HiltonHiltz <150003836+HiltonHiltz@users.noreply.github.com>
2024-02-11 19:37:48 +0200

    first commit

---
+++ src/providers/data/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { DataProvider } from "@refinedev/core";
+
+export const dataProvider = (url: string): DataProvider => ({
+  getOne: async ({ id, resource }) => {
+    const response = await fetch(`${url}/${resource}/${id}`);
+      const data = await response.json();
+
+      return {
+          data,
+      };
+  },
+
+  create: async () => {
+      throw new Error("Not implemented");
+  },
+  update: async () => {
+      throw new Error("Not implemented");
+  },
+  deleteOne: async () => {
+      throw new Error("Not implemented");
+  },
+  getList: async () => {
+      throw new Error("Not implemented");
+  },
+  getApiUrl: () => url,
+});
